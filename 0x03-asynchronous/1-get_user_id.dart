8db8d48a3791a8f86865e5f0869d842e7ba0b3d2
BradenE19 <6115@holbertonstudents.com>
2024-06-21 15:52:24 -0500

    task1 get user id

---
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -0,0 +1,8 @@
+import 'dart:convert';
+import '1-util.dart';
+
+Future<String> getUserId() async {
+  String userDataJson = await fetchUserData();
+  Map<String, dynamic> userData = jsonDecode(userDataJson);
+  return userData['id'];
+}
