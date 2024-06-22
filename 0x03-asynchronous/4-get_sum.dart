2f50a315417a3b97dcb39611211af45475ff793a
BradenE19 <6115@holbertonstudents.com>
2024-06-22 15:49:33 -0500

    i cannot figure this out task4 attempt

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,29 +1,20 @@
-import 'dart:convert';
 import '4-util.dart';
+import 'dart:convert';
 
-Future<int> calculateTotal() async {
+calculateTotal() async {
   try {
-    // Fetch user data
-    String data = await fetchUserData();
-    Map<String, dynamic> dataMap = jsonDecode(data);
-
-    // Fetch user orders
-    String userOrderJson = await fetchUserOrders(dataMap['id']);
-    List<dynamic> productsUser = jsonDecode(userOrderJson);
-
-    double sumPro = 0.0;
-    for (String product in productsUser) {
-      // Fetch product price
-      String productPriceJson = await fetchProductPrice(product.trim());
-      dynamic productPrice = jsonDecode(productPriceJson);
-
-      // Ensure product price is treated as double
-      sumPro += (productPrice is int) ? productPrice.toDouble() : productPrice;
+    var user_data = await fetchUserData();
+    var user_id = jsonDecode(user_data)['id'];
+    var order = await fetchUserOrders(user_id);
+    List<dynamic> product_list = jsonDecode(order);
+    var price = 0.0;
+    for (int i = 0; i < product_list.length; i++) {
+      var pri = await fetchProductPrice(product_list[i]);
+      price += num.parse(pri);
     }
 
-    return sumPro.toInt();
-  } catch (e) {
-    print("error caught: $e");
+    return price;
+  } catch (error) {
     return -1;
   }
 }
