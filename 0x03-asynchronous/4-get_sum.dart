973f9ee6eeddb5e6a8acb69ecb05ed9fb6920910
BradenE19 <6115@holbertonstudents.com>
2024-06-22 15:38:55 -0500

    task4 attempt to fix output error

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,23 +1,24 @@
-import "4-util.dart";
-import "dart:convert";
+import 'dart:convert';
+
+import '4-util.dart';
 
 Future<double> calculateTotal() async {
   try {
-  
-    return fetchUserData().then((user) {
-      
-      String id = json.decode(user)["id"];
- 
-      return fetchUserOrders(id).then((products) async {
-        double totalPrice = 0.0;
-        for (String product in json.decode(products)) {
-          await fetchProductPrice(product).then((price) {
-            totalPrice += json.decode(price);
-          });
-        }
-        return totalPrice;
-      });
-    });
+    String data = await fetchUserData();
+
+    Map dataMap = jsonDecode(data);
+
+    String userOrder = await fetchUserOrders(dataMap['id']);
+    userOrder = userOrder.substring(1, userOrder.length - 1);
+    List<String> productsUser = userOrder.split(',');
+
+    double sumPro = 0;
+    for (String product in productsUser) {
+      product = product.substring(1, product.length - 1);
+      sumPro += double.parse(await fetchProductPrice(product));
+    }
+
+    return sumPro;
   } catch (e) {
     return -1;
   }
